386d0a75939e8e114685312e6ce691452307425b
Kaleakash <akash300383@gmail.com>
2021-03-08 12:06:30 +0530

    I Done some changes

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-       System.out.println("This project pull from git repository.....");
+       System.out.println("This project pull from git repository.....");	
+	Ssytem.out.println("I Pushed new changes in my code");
     }
 }
